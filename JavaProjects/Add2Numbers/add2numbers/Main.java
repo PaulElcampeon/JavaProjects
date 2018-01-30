cbfc8a196db2eddc620cf9243d9da1fcf471c0ce
Paul Oladele <pauloladele@live.co.uk>
2018-01-30 20:45:45 +0000

    1st commit for add2Numbers

--- JavaProjects/Add2Numbers/add2numbers/Main.java
+++ JavaProjects/Add2Numbers/add2numbers/Main.java
@@ -1,5 +1,21 @@
 package add2numbers;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please Enter your first number");
+		double firstNum = input.nextDouble();
+		
+		System.out.println("Enter your second number");
+		double secondNum = input.nextDouble();
+		
+		double answer = firstNum + secondNum;
+		
+		System.out.println("Your answer is "+answer);
+		
+	}
 }
