e30cf7872e26caf8d0e1d9e95e4597ba471695ac
Paul Oladele <pauloladele@live.co.uk>
2018-01-31 15:28:05 +0000

    stills needs a little working but most of it is done

--- JavaProjects/Statistics/statistics/Gui.java
+++ JavaProjects/Statistics/statistics/Gui.java
@@ -1,5 +1,6 @@
 package statistics;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -27,16 +28,147 @@ public class Gui extends JFrame implements ActionListener{
 		
 		instructions = new JLabel("Enter your values seperated by a comma");
 		instructions.setHorizontalAlignment(JLabel.CENTER);
+		instructions.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		instructions.setBounds(43, 11, 478, 30);
+		panel.add(instructions);
 		
+		data = new JTextField();
+		data.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		data.setBounds(22, 59, 536, 41);
+		panel.add(data);
 		
+		mean = new JButton("MEAN");
+		mean.setFont(new Font("Tahoma", Font.BOLD, 20));
+		mean.setBounds(41,128,121,41);
+		mean.addActionListener(this);
+		panel.add(mean);
 		
-	}
+		median = new JButton("MEDIAN");
+		median.setFont(new Font("Tahoma", Font.BOLD, 20));
+		median.setBounds(172,128,121,41);
+		median.addActionListener(this);
+		panel.add(median);
+		
+		mode = new JButton("MODE");
+		mode.setFont(new Font("Tahoma", Font.BOLD, 20));
+		mode.setBounds(303,128,121,41);
+		mode.addActionListener(this);
+		panel.add(mode);
+		
+		range = new JButton("RANGE");
+		range.setFont(new Font("Tahoma", Font.BOLD, 20));
+		range.setBounds(434,128,121,41);
+		range.addActionListener(this);
+		panel.add(range);
+		
+		lq = new JButton("LOWER QUARTILE");
+		lq.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lq.setBounds(43,188,234,41);
+		lq.addActionListener(this);
+		panel.add(lq);
+		
+		uq = new JButton("UPPER QUARTILE");
+		uq.setFont(new Font("Tahoma", Font.BOLD, 20));
+		uq.setBounds(283,188,275,41);
+		uq.addActionListener(this);
+		panel.add(uq);
+		
+		var = new JButton("VARIANCE");
+		var.setFont(new Font("Tahoma", Font.BOLD, 20));
+		var.setBounds(46,252,193,41);
+		var.addActionListener(this);
+		panel.add(var);
+		
+		std = new JButton("STANDARD DEVIATION");
+		std.setFont(new Font("Tahoma", Font.BOLD, 20));
+		std.setBounds(249,252,309,41);
+		std.addActionListener(this);
+		panel.add(std);
+		
+		answer = new JLabel("Your answer will appear here");
+		answer.setHorizontalAlignment(SwingConstants.CENTER);
+		answer.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		answer.setBounds(22, 329, 536, 41);
+		panel.add(answer);
+		
+		super.add(panel);
+		
+		super.setTitle("Statistics - Averages");
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setSize(600,450);
+		super.setResizable(false);
+		super.setVisible(true);
 	
 	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		Methods method = new Methods();
+		String DATA = data.getText().trim();
+		
+		if(DATA.matches(".*[a-z].*") || DATA.matches("")) {
+			JOptionPane.showMessageDialog(this, "You cannot have letters in your fields please correct this.");
+		}
+		
+		else {
+
+		String[] DATAnum = DATA.split("[,]");
+		
+		if(checker1(DATAnum) == true ) {
+			JOptionPane.showMessageDialog(this, "You have left some blank spaces, please correct this.");
+		}
+		
+		else {
+		
+		double[] dataNum = new double[DATAnum.length];
+		
+		for(int i = 0; i<DATAnum.length; i++) {
+			dataNum[i] = Double.parseDouble(DATAnum[i]);
+		}
+		
+		if(event.getSource() == mean) {
+			answer.setText("The mean is "+method.meaN(dataNum));
+		}
+		
+		if(event.getSource() == median) {
+			answer.setText("The median is "+method.mediaN(dataNum));
+		}
+		
+		if(event.getSource() == mode) {
+			answer.setText("The mode is "+method.modE(dataNum));
+		}
+		
+		if(event.getSource() == range) {
+			answer.setText("The range is "+method.rangE(dataNum));
+		}
+		
+		if(event.getSource() == lq) {
+			answer.setText("The Lower Quartile is "+method.LowerQuarT(dataNum));
+		}
+	
+		if(event.getSource() == uq) {
+			answer.setText("The Upper Quartile is "+method.UpperQuarT(dataNum));
+		}
+		
+		if(event.getSource() == var) {
+			answer.setText("The variance is "+method.variancE(dataNum));
+		}
+		
+		if(event.getSource() == std) {
+			answer.setText("The standard deviation is "+method.StandardDeV(dataNum));
+			}
+		}
+	}
+	}
+
+		public static boolean checker1(String[] a) {
+
+			for (String s: a) {
+		        if (s.equals("")) {
+		            return true;
+		        }
+		}
+		return false;
+	}
 }
--- JavaProjects/Statistics/statistics/Methods.java
+++ JavaProjects/Statistics/statistics/Methods.java
@@ -1,5 +1,174 @@
 package statistics;
 
+import java.util.Arrays;
+
 public class Methods {
 
-}
+	public double meaN(double [] a) {
+		double sum = 0;
+		double ans = 0; 
+		
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		
+		ans = sum/a.length;
+		
+		return ans;
+	}
+	
+	
+	public double mediaN(double [] a) {
+		int count = a.length;
+		Arrays.sort(a);
+		double ans = 0;
+	
+		if(a.length % 2 == 0) {
+			double num = Math.ceil(a.length/2);
+			int numx = (int) num;
+			return a[numx];
+		}
+		
+		else
+		{
+			ans = (a[a.length/2] + a[(a.length/2) + 1])/2;
+			return ans;
+		}
+	}
+	
+	public String modE(double [] a) {
+		Arrays.sort(a);
+		String modex = "";
+		double countx = 0;
+	
+		for(int i = 0; i<a.length; i++) {
+			int county = 0;
+			for(int j = 0; j<a.length; j++) {
+					if(a[i] == a[j]) {
+						county++;
+						if(!(modex.contains(Double.toString(a[i])))){
+						if(county == countx) {
+							modex += " and "+Double.toString(a[i]);
+							}
+						}
+						if(county > countx) {
+							modex = Double.toString(a[i]);
+							countx = county;
+						}
+					}
+				}
+			}
+
+				return modex ;
+		}
+		
+	public double rangE(double [] a) {
+		Arrays.sort(a);
+		int count = a.length;
+		
+		double rangex = a[a.length-1] - a[0];
+		
+		return rangex;
+	}
+	
+	public double LowerQuarT(double [] a) {
+		int count = a.length;
+		Arrays.sort(a);
+		double ans = 0;
+	
+		if((a.length+1) % 4 == 1) {
+			double num = Math.ceil((a.length+1)/4);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx] - a[numy];
+			double diff2 = diff*0.25;
+			double ANS = a[numy] + diff2; 
+			return ANS;
+		}
+		
+		else if((a.length+1) % 4 == 2){
+			double num = Math.ceil((a.length+1)/4);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx] - a[numy];
+			double diff2 = diff*0.5;
+			double ANS = a[numy] + diff2; 
+			return ANS;
+		}
+		else if((a.length+1) % 4 == 3) {
+			double num = Math.ceil((a.length+1)/4);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx] - a[numy];
+			double diff2 = diff*0.75;
+			double ANS = a[numy] + diff2; 
+			return ANS;
+		}
+		return ans;
+	}
+	
+	public double UpperQuarT(double [] a) {
+		int count = a.length;
+		Arrays.sort(a);
+		double ans = 0;
+		
+		if((a.length+1) % 4 == 1) {
+			double num = Math.ceil((a.length+1)*0.75);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx-1] - a[numy-1];
+			double diff2 = diff*0.75;
+			double ANS = a[numy-1] + diff2; 
+			return ANS;
+		}
+		
+		else if((a.length+1) % 4 == 2){
+			double num = Math.ceil((a.length+1)*0.75);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx-1] - a[numy-1];
+			double diff2 = diff*0.5;
+			double ANS = a[numy-1] + diff2; 
+			return ANS;
+		}
+		else if((a.length+1) % 4 == 3) {
+			double num = Math.ceil((a.length+1)*0.75);
+			double num1 = num - 1;
+			int numx = (int) num;
+			int numy = (int) num1;
+			double diff = a[numx-1] - a[numy-1];
+			double diff2 = diff*0.25;
+			double ANS = a[numy-1] + diff2; 
+			return ANS;
+		}
+		return ans;
+	}
+		
+	public double variancE(double [] a) {
+		double sumX = 0;
+		double sumX2 = 0;
+		double ans = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			sumX2 += Math.pow(a[i], 2);
+		}
+		
+		for(int i = 0; i < a.length; i++) {
+			sumX += a[i];
+		}
+		
+		ans = (sumX2/a.length) - (Math.pow((sumX/a.length), 2));
+		return ans;
+	}
+
+	public double StandardDeV(double[] a) {
+		double ans = Math.sqrt(variancE(a));
+		return ans;
+		}
+	}
+
