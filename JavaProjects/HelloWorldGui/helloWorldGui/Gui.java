4bbb3139ec1b6d1b886231542c5266192d14b9dd
Paul Oladele <pauloladele@live.co.uk>
2018-01-31 07:35:09 +0000

    set fonts and coordinates for button and label

--- JavaProjects/HelloWorldGui/helloWorldGui/Gui.java
+++ JavaProjects/HelloWorldGui/helloWorldGui/Gui.java
@@ -1,9 +1,14 @@
 package helloWorldGui;
 
+import java.awt.Font;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+/*This program will create a GUI with a a Label and a button. 
+ * When the user clicks the button a message ("Hello World") will appear 
+ * on the label */
 public class Gui extends JFrame implements ActionListener{
 	
 	JPanel panel = new JPanel();
@@ -12,13 +17,27 @@ public class Gui extends JFrame implements ActionListener{
 	
 	public Gui() {
 		
+		panel.setLayout(null);
+		
+		messageBoard.setBounds(0, 0, 394, 153);
+		messageBoard.setFont(new Font("Tahoma", Font.BOLD, 40));
+		panel.add(messageBoard);
+		
+		msgBoardbtn.setBounds(93, 185, 188, 50);
+		msgBoardbtn.setText("Click Me");
+		msgBoardbtn.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		msgBoardbtn.addActionListener(this);
+		panel.add(msgBoardbtn);
+		
+		super.add(panel);
+		
 		super.setTitle("Hello World");
-		super.setVisible(true);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setSize(400, 300);
 		super.setResizable(false);
 		super.setLocationRelativeTo(null);
-		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
+		super.setVisible(true);
+
 		
 	}
 		
