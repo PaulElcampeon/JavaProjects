ff04de171ff0ab7c58e0b65a4aa915d8c09b376b
Paul Oladele <pauloladele@live.co.uk>
2018-01-31 08:01:18 +0000

    setting up for the add2NumbersGUI

---
+++ JavaProjects/Add2NumbersGui/adding2Numbers/MainWithGUI.java
@@ -0,0 +1,22 @@
+package adding2Numbers;
+import javax.swing.*;
+
+/*This programme allows user to input two numbers and on the click of a 
+ * button the sum of the two numbers I will be displayed on a JLabel.*/
+public class MainWithGUI{
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField num1 = new JTextField();
+	JTextField num2 = new JTextField();
+	JButton addbtn = new JButton("Add");
+	JLabel ansLabel = new JLabel("Your answer will appear here");
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
