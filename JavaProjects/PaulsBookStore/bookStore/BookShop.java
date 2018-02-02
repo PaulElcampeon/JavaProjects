4178818d340f2dcd24b01d90a4d57dccae9d8a0b
Paul Oladele <pauloladele@live.co.uk>
2018-02-02 19:49:07 +0000

    Beginning Bookshop Project

---
+++ JavaProjects/PaulsBookStore/bookStore/BookShop.java
@@ -0,0 +1,15 @@
+package bookStore;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class BookShop extends JFrame implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
